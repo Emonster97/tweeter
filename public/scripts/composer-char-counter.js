b999973ca7572a8cc29ab97081a4013b55a81640
Wilfred <wilfredemonts@gmail.com>
2021-12-16 17:05:21 -0500

    cleaned up the header and nav to properly overlap and display with each other, created example tweets and styled them on the page, built and attached a ccounter function for the counter element displaying the number of characters a new tweet will contain, turning red if you have run out of characters and displaying a negative count as well

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
+
+$(document).ready(function() { 
   // --- our code goes here ---
+  $("#tweet-text").on("input", function() {
+    let count = 140 - this.value.length;
+    let counter = $(this).parent().parent().find(".counter")[0];
+    counter.innerHTML = count;
+    if (count < 0) {
+      $(counter).css("color", "red");
+    } else {
+      $(counter).css("color", "unset");
+    }
+    console.log(counter);
+    console.log(count);
+  });
 });
+
+
